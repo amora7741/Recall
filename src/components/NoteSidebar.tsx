f1ba70449dadea50a749ac624645e04c5e62caec
amora7741 <mora7741@gmail.com>
2025-05-13 14:07:18 -0700

    Hide sidebar if no user

--- src/components/NoteSidebar.tsx
+++ src/components/NoteSidebar.tsx
@@ -1,24 +1,11 @@
 import { getUser } from "@/utils/supabase/server";
-import { Button } from "./ui/button";
-import Link from "next/link";
 import { prisma } from "@/db/prisma";
 import { Note } from "@prisma/client";
 
 const NoteSidebar = async () => {
   const user = await getUser();
 
-  if (!user) {
-    return (
-      <div className="grid w-80 place-items-center border-r bg-muted p-4 sm:p-8">
-        <div className="flex flex-col items-center gap-2">
-          <p className="text-lg">Log in to view your notes!</p>
-          <Button asChild>
-            <Link href="/login">Log In</Link>
-          </Button>
-        </div>
-      </div>
-    );
-  }
+  if (!user) return null;
 
   const notes: Note[] = await prisma.note.findMany({
     where: { authorId: user.id },
